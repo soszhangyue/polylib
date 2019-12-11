d64f2b147fe30e3e78ec0ae624f859b5ff94a8cd
zhangyue <soszhangyue@buaa.edu.cn>
2019-12-11 10:31:13 +0800

    update Hermite

--- python_version/Hermite.py
+++ python_version/Hermite.py
@@ -1,6 +1,8 @@
-import numpy as np 
+import numpy as np
 import math
 from gPCBasic import *
+from scipy.special import gamma
+
 
 def HermiteCoef(order):
     """
@@ -10,11 +12,13 @@ def HermiteCoef(order):
     """
 
     coef = np.zeros([1, order + 1])
-    for index_coef in range(int(np.round((order-0.1)/2)) + 1):
+    for index_coef in range(int(np.round((order - 0.1) / 2)) + 1):
+        coef[0, order - 2 * index_coef] = np.power((-1), index_coef) / np.power(2, index_coef) * binom_coef(order,
+                                                                                                            index_coef) * np.prod(
+            lin_as_mat((order - 2 * index_coef + 1), (order - index_coef)))
 
-        coef[0,order - 2 * index_coef] = np.power((-1), index_coef)/np.power(2, index_coef) * binom_coef(order, index_coef) * np.prod(lin_as_mat((order-2*index_coef+1), (order-index_coef)))
-    
     return np.fliplr(coef)
+
 
 def HermiteCoef_t(order, t):
     """
@@ -22,12 +26,13 @@ def HermiteCoef_t(order, t):
 
     """
 
-    coef  = HermiteCoef(order)
-    cnt=0
-    for index in range(1, order + 1 +2, 2):
+    coef = HermiteCoef(order)
+    cnt = 0
+    for index in range(1, order + 1 + 2, 2):
         coef[0, index - 1] = coef[0, index - 1] * np.power(t, cnt)
-        cnt =cnt +1
+        cnt = cnt + 1
     return coef
+
 
 def HermiteF(x, degree):
     """
@@ -38,61 +43,85 @@ def HermiteF(x, degree):
     elif degree == 1:
         return x
     else:
-        return x * HermiteF(x, degree-1) - (degree-1) * HermiteF(x, degree-2)
+        return x * HermiteF(x, degree - 1) - (degree - 1) * HermiteF(x, degree - 2)
 
-def HermiteF_nd(x:np.ndarray, ndim, norder):
+
+def HermiteF_nd(x: np.ndarray, ndim, norder):
     ntmp = x.shape[1]
 
     if ntmp != ndim:
         print('Incompatible dimension in HermiteF_nd! Quit.\n')
-    pmatrix = chaos_sequence(ndim,norder)
-    ptmp = np.zeros([norder+1,ndim])
+    pmatrix = chaos_sequence(ndim, norder)
+    ptmp = np.zeros([norder + 1, ndim])
 
     for index in range(0, norder + 1, 1):
-        ptmp[index,:] = HermiteF(x,index)
+        ptmp[index, :] = HermiteF(x, index)
     nterm = pmatrix.shape[0]
     poly = np.ones([1, nterm])
 
     for index_m in range(2, nterm + 1, 1):
         for index_n in range(1, ndim + 1, 1):
-            poly[0, index_m - 1] = poly[0 ,index_m - 1]  * ptmp[int(pmatrix[index_m - 1,index_n - 1]),index_n - 1]
+            poly[0, index_m - 1] = poly[0, index_m - 1] * ptmp[int(pmatrix[index_m - 1, index_n - 1]), index_n - 1]
 
     return poly
-def HermiteD(x, degree):
 
+
+def HermiteD(x, degree):
     if degree == 0:
         return np.zeros(np.size(x))
     elif degree == 1:
         return np.ones(np.size(x))
     else:
-        return degree * HermiteF(x, degree -1)
+        return degree * HermiteF(x, degree - 1)
+
 
 def HermiteD_nd(x, ndim, norder, nder):
     ntmp = x.shape[1]
 
     if ntmp != ndim:
         print('Incompatible dimension in HermiteD_nd! Quit.\n')
-    pmatrix = chaos_sequence(ndim,norder)
-    ptmp = np.zeros([norder+1,ndim])
-    pder = np.zeros([norder+1,ndim])
+    pmatrix = chaos_sequence(ndim, norder)
+    ptmp = np.zeros([norder + 1, ndim])
+    pder = np.zeros([norder + 1, ndim])
     for index in range(0, norder + 1, 1):
-        ptmp[index,:] = HermiteF(x,index)
-        pder[index,:] = HermiteD(x,index)
+        ptmp[index, :] = HermiteF(x, index)
+        pder[index, :] = HermiteD(x, index)
 
     nterm = pmatrix.shape[0]
     poly = np.ones([1, nterm])
-    poly[0,0] = 1
+    poly[0, 0] = 1
 
     for index_m in range(2, nterm + 1, 1):
         for index_n in range(1, ndim + 1, 1):
             if index_n == nder:
-                poly[0, index_m - 1] = poly[0 ,index_m - 1]  * pder[int(pmatrix[index_m - 1,index_n - 1]),index_n - 1]
+                poly[0, index_m - 1] = poly[0, index_m - 1] * pder[int(pmatrix[index_m - 1, index_n - 1]), index_n - 1]
             else:
-                poly[0, index_m - 1] = poly[0 ,index_m - 1]  * ptmp[int(pmatrix[index_m - 1,index_n - 1]),index_n - 1]
-            
+                poly[0, index_m - 1] = poly[0, index_m - 1] * ptmp[int(pmatrix[index_m - 1, index_n - 1]), index_n - 1]
+
     return poly
+
+
+def HermiteRing_1d(s, l):
+    c = np.zeros([s + l + 1, 1])
+    for index in range(np.abs(s - l), (s + l) + 1, 1):
+        g = (l + s + r) / 2
+        if np.abs(g - np.round(g)) < 0.0001:
+            c[index + 1, 1] = gamma(s + 1) * gamma(l + 1) / (gamma(g - l + 1) * gamma(g - s + 1) * gamma(g - index + 1))
+    return c
+
+
+def HermiteZeros(degree):
+    z = np.roots(HermiteCoef(degree))
+    return z
+
+
+def HermiteZeros_t(degree, t):
+    z = np.roots(HermiteCoef_t(degree, t))
+    return z
+
+
 if __name__ == "__main__":
-    x = lin_as_mat(1,1.3,0.1)
+    x = lin_as_mat(1, 1.3, 0.1)
     order = 3
-    exp = HermiteF_nd(x,4,order)
+    exp = HermiteF_nd(x, 4, order)
     print(exp)
